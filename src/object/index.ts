642e9eaf9b8a8d51be51fca4f29e23bdde0baf60
asosunoff <al.sosunoff@gmail.com>
2021-12-04 14:20:43 +0300

    fix

--- src/object/index.ts
+++ src/object/index.ts
@@ -1,3 +1,5 @@
 export type { Merge } from "./merge";
 export type { isObject } from "./is-object";
 export type { PropsPath } from "./props-path";
+export type { CollapseObject } from "./collapse-object";
+export type { GetTypeByPropsPath } from "./get-type-by-props-path";
