85447bec88092bb5ec7717c1834e3348e605567f
asosunoff <al.sosunoff@gmail.com>
2026-06-05 05:56:24 -0300

    updated index

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,36 @@ import * as object from "./object";
 import * as common from "./common";
 
 export { array, string, object, any, common };
+
+export type { First as ArrayFirst } from "./array/first";
+export type { Join as ArrayJoin } from "./array/join";
+export type { Last as ArrayLast } from "./array/last";
+export type { Length as ArrayLength } from "./array/length";
+export type { Pop as ArrayPop } from "./array/pop";
+export type { Push as ArrayPush } from "./array/push";
+export type { Shift as ArrayShift } from "./array/shift";
+export type { Unshift as ArrayUnshift } from "./array/unshift";
+
+export type { Anyfy } from "./any/anyfy";
+
+export type { EmitListeners } from "./common/emit-listeners";
+export type { IsFunction } from "./common/is-function";
+export type { MaybeRefProps } from "./common/maybe-ref-props";
+export type { Prettify } from "./common/prettify";
+export type { ReplaceAll as CommonReplaceAll } from "./common/replace-all";
+export type { SplitCamelCase } from "./common/split-camel-case";
+
+export type { CollapseObject } from "./object/collapse-object";
+export type { GetTypeByPropsPath } from "./object/get-type-by-props-path";
+export type { isObject } from "./object/is-object";
+export type { Merge } from "./object/merge";
+export type { PropsPath } from "./object/props-path";
+
+export type { At as StringAt } from "./string/At";
+export type { First as StringFirst } from "./string/first";
+export type { Last as StringLast } from "./string/last";
+export type { Length as StringLength } from "./string/length";
+export type { Replace as StringReplace } from "./string/replace";
+export type { ReplaceAll as StringReplaceAll } from "./string/replace-all";
+export type { Split as StringSplit } from "./string/split";
+export type { Tail as StringTail } from "./string/tail";
